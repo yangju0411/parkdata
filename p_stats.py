a88fac7f5e1de80f6f951a63782d97f8c40f67ce
DESKTOP-8EFB7PS\pdgzms <yangju0411@gmail.com>
2020-11-16 19:39:52 +0900

    독립 t까지 구현 완료; 독립 t 결과창에 등분산 검정 결과도 표시할 것, 선형회귀, 상관분석 추가할 것, PCA는 하지 마셈

--- p_stats.py
+++ p_stats.py
@@ -20,7 +20,8 @@ class One_Ttest:
                             "Variance" : round(self.var_x, 3),
                             "Test_value" : self.value,
                             "T" : round(self.t, 3),
-                            "df" : self.df, "p_value" : round(self.p, 3)},
+                            "df" : self.df,
+                            "p_value" : round(self.p, 3)},
                            index = [0])
         return rdf
     
@@ -35,25 +36,60 @@ class Paired_Ttest:
             self.d = self.mean_after - self.mean_before
             self.var_d = np.var(after - before)
             self.df = len(before) - 1
-            try:
-                self.t = (self.d - self.value) / np.sqrt(self.var_d / len(before))
-            except:
-                pass #분모가 0이 될 경우 처리하
+            self.t = (self.d - self.value) / np.sqrt(self.var_d / len(before))
             self.p = (1 - stats.t.cdf(abs(self.t), df = self.df)) * 2
             
         
-        def result(self):
-            rdf = pd.DataFrame({"Mean_Before" : round(self.mean_before, 3),
-                                "Mean_After" : round(self.mean_after, 3),
-                                "Mean_Diff" : round(self.d, 3),
-                                "Variance_Diff" : round(self.var_d, 3),
-                                "Test_value" : self.value,
-                                "T" : round(self.t, 3),
-                                "df" : self.df,
-                                "p_value" : round(self.p, 3)},
-                               index = [0])
-            return rdf
-            
+    def result(self):
+        rdf = pd.DataFrame({"Mean_Before" : round(self.mean_before, 3),
+                            "Mean_After" : round(self.mean_after, 3),
+                            "Mean_Diff" : round(self.d, 3),
+                            "Variance_Diff" : round(self.var_d, 3),
+                            "Test_value" : self.value,
+                            "T" : round(self.t, 3),
+                            "df" : self.df,
+                            "p_value" : round(self.p, 3)},
+                           index = [0])
+        return rdf
+        
             
                 
+class Inde_Ttest:
+    def __init__(self, x, group, value = 0):
+        if group.nunique() != 2:
+            print("올바른 그룹 변수가 아닙니다.")
+        else:
+            self.value = value
+            groupby = x.groupby(group)
+            self.mean_x1 = groupby.mean().iloc[0]
+            self.mean_x2 = groupby.mean().iloc[1]
+            self.n_x1 = groupby.size().iloc[0]
+            self.n_x2 = groupby.size().iloc[1]
+            self.var_x1 = groupby.var().iloc[0]
+            self.var_x2 = groupby.var().iloc[1]       
+            self.d = self.mean_x1 - self.mean_x2
+            
+            if self.var_x1 / self.var_x2 <= 1:
+                self.f = self.var_x2 / self.var_x1
+                self.f_p = 2 * (1 - stats.f.cdf(self.f, self.n_x2 - 1, self.n_x1 - 1))
+            else:
+                self.f = self.var_x1 / self.var_x2
+                self.f_p = 2 * (1 - stats.f.cdf(self.f, self.n_x1 - 1, self.n_x2 - 1))
+            
+            self.t_e = (self.d - self.value) / np.sqrt((self.var_x1 / self.n_x1) + (self.var_x2 / self.n_x2))
+            
+            var_p = ((self.n_x1 - 1) * self.var_x1 + (self.n_x2 - 1) * self.var_x2) / (self.n_x1 + self.n_x2 - 2)
+            self.t_ne = (self.d - self.value) / np.sqrt((var_p / self.n_x1) + (var_p / self.n_x2))
+            
+            self.p_e = (1 - stats.t.cdf(abs(self.t_e), df = self.n_x1 + self.n_x2 - 2)) * 2
+            self.p_ne = (1 - stats.t.cdf(abs(self.t_ne), df = self.n_x1 + self.n_x2 - 2)) * 2
+            
+            
+    def result(self):
+        rdf = pd.DataFrame({"Test_value" : [self.value, ""],
+                            "T" : [round(self.t_e, 3), round(self.t_ne, 3)],
+                            "df" : [self.n_x1 + self.n_x2 - 2, ""],
+                            "p_value" : [round(self.p_e, 3), round(self.p_ne, 3)]})
+        rdf.index = ["등분산", "이분산"]
+        return rdf
         
--- read_data.py
+++ read_data.py
@@ -7,6 +7,8 @@ from tkinter import filedialog
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
+import p_stats
+
 class readData:
     def __init__(self, Main):      
         self.main = Main
@@ -58,8 +60,18 @@ class readData:
         var_list.pack()  
 
     def one_ttest(self):
+        def ttest():
+            col_index = int(var_list.curselection()[0])
+            value = float(value_Entry.get())
+            tt = p_stats.One_Ttest(x = self.df.iloc[:, col_index], value = value)
+            print(tt.var_x)
+            print(tt.t)
+            print(tt.p)
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                self.main.report(tt.result())
+                
         variables = self.df.columns
-        one_ttest_menu = Toblevel()
+        one_ttest_menu = Toplevel()
         one_ttest_menu.title("단일표본 t검정")
         one_ttest_menu.geometry("500x250")
         
@@ -67,14 +79,98 @@ class readData:
         
         for i in variables:
             var_list.insert(END, i)
-            
-        mu = Entry(one_ttest_menu)
         
-        pass
+        print_bt = Button(one_ttest_menu, text = "확인", command = ttest)
+        
+        value_Label = Label(one_ttest_menu, text = "검정값")
+        value_Entry = Entry(one_ttest_menu)
+        value_Entry.insert(0, "0")
+        
+        var_list.grid(row = 0, column = 1)
+        value_Label.grid(row = 1, column = 0)
+        value_Entry.grid(row = 1, column = 1)
+        print_bt.grid(row = 2, column = 1)
+        
     def pair_ttest(self):
-        pass
+        def ttest():
+            col1_index = int(before_LB.curselection()[0])
+            col2_index = int(after_LB.curselection()[0])
+            if col1_index != col2_index:
+                value = float(value_Entry.get())
+                tt = p_stats.Paired_Ttest(before = self.df.iloc[:, col1_index], after = self.df.iloc[:, col2_index], value = value)
+                print(tt.t)
+                print(tt.p)
+                with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                    self.main.report(tt.result())
+            else:
+                self.main.report("같은 변수를 선택하였습니다.")
+                
+        variables = self.df.columns
+        pair_ttest_menu = Toplevel()
+        pair_ttest_menu.title("대응표본 t검정")
+        pair_ttest_menu.geometry("500x250")
+        
+        before_Label = Label(pair_ttest_menu, text = "변수 1")
+        after_Label = Label(pair_ttest_menu, text = "변수 2")
+        before_LB = Listbox(pair_ttest_menu, selectmode = "single", exportselection=0)
+        after_LB = Listbox(pair_ttest_menu, selectmode = "single", exportselection=0)
+        
+        for i in variables:
+            before_LB.insert(END, i)
+            after_LB.insert(END, i)
+        
+        print_bt = Button(pair_ttest_menu, text = "확인", command = ttest)
+        
+        value_Label = Label(pair_ttest_menu, text = "검정값")
+        value_Entry = Entry(pair_ttest_menu)
+        value_Entry.insert(0, "0")
+        
+        before_Label.grid(row = 0, column = 0)
+        after_Label.grid(row = 0, column = 1)
+        before_LB.grid(row = 1, column = 0)
+        after_LB.grid(row = 1, column = 1)
+        value_Label.grid(row = 2, column = 0)
+        value_Entry.grid(row = 2, column = 1)
+        print_bt.grid(row = 3, column = 1)
+    
     def in_ttest(self):
-        pass
+        def ttest():
+            var_index = int(before_LB.curselection()[0])
+            group_index = int(after_LB.curselection()[0])
+            value = float(value_Entry.get())
+            tt = p_stats.Inde_Ttest(x = self.df.iloc[:, var_index], group = self.df.iloc[:, group_index], value = value)
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                self.main.report(tt.result())
+
+                
+        variables = self.df.columns
+        in_ttest_menu = Toplevel()
+        in_ttest_menu.title("독립표본 t검정")
+        in_ttest_menu.geometry("500x250")
+        
+        before_Label = Label(in_ttest_menu, text = "변수")
+        after_Label = Label(in_ttest_menu, text = "그룹 변수")
+        before_LB = Listbox(in_ttest_menu, selectmode = "single", exportselection=0)
+        after_LB = Listbox(in_ttest_menu, selectmode = "single", exportselection=0)
+        
+        for i in variables:
+            before_LB.insert(END, i)
+            after_LB.insert(END, i)
+        
+        print_bt = Button(in_ttest_menu, text = "확인", command = ttest)
+        
+        value_Label = Label(in_ttest_menu, text = "검정값")
+        value_Entry = Entry(in_ttest_menu)
+        value_Entry.insert(0, "0")
+        
+        before_Label.grid(row = 0, column = 0)
+        after_Label.grid(row = 0, column = 1)
+        before_LB.grid(row = 1, column = 0)
+        after_LB.grid(row = 1, column = 1)
+        value_Label.grid(row = 2, column = 0)
+        value_Entry.grid(row = 2, column = 1)
+        print_bt.grid(row = 3, column = 1)
+        
     def lr(self):
         pass
     def pca(self):
